615f20c070cce4fa1d4c2217013dfdd174e46d6d
lijun19940913 <18600334403@163.com>
2018-09-05 00:21:11 +0800

    first commot

---
+++ app/src/main/java/firsttest/lijun/com/firsttest/MainActivity.java
@@ -0,0 +1,64 @@
+package firsttest.lijun.com.firsttest;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.*;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText edit;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edit=(EditText) findViewById(R.id.text1);
+        button=(Button) findViewById(R.id.button1);
+        button.setOnClickListener(this);
+    }
+
+    public void save(String inputText){
+        FileOutputStream out=null;
+        Writer writer=null;
+        try{
+            out=openFileOutput("data", Context.MODE_PRIVATE);
+            writer=new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(inputText);
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if (writer!=null){
+                    writer.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    protected void onDestory(){
+        super.onDestroy();
+        String inputText=edit.getText().toString();
+        save(inputText);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button1:
+                Intent intent=new Intent(MainActivity.this,SecendActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+
+        }
+    }
+
+}
---
+++ app/src/main/java/firsttest/lijun/com/firsttest/SecendActivity.java
@@ -0,0 +1,68 @@
+package firsttest.lijun.com.firsttest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.*;
+
+public class SecendActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView edit;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secend);
+        edit=(TextView) findViewById(R.id.TextView1);
+        String inputText=load();
+        if (!TextUtils.isEmpty(inputText)){
+            edit.setText(inputText);
+            Toast.makeText(this,"restoring is suceeded",Toast.LENGTH_SHORT).show();
+        }
+        button=(Button) findViewById(R.id.button2);
+        button.setOnClickListener(this);
+
+    }
+
+    public String load(){
+        FileInputStream in=null;
+        BufferedReader reader=null;
+        StringBuilder content=new StringBuilder();
+        try{
+            in=openFileInput("data");
+            reader=new BufferedReader(new InputStreamReader(in));
+            String line="";
+            while ((line=reader.readLine())!=null){
+                content.append(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (reader!=null){
+                try {
+                    reader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button2:
+                Intent intent=new Intent(SecendActivity.this,MainActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}
